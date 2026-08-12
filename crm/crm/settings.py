dfb0ae96206679cadc7cecc74075ee3418363392
axatevenmore-spec <axat.evenmore@gmail.com>
2026-08-12 10:02:48 +0530

    Dev crm axat (#4)
    * fix : path fixed in setting

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR.parent / ".env")
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
