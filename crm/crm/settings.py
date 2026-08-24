858526d13d69845d9afa5c5303e980e04ca732c1
Axat <axat.evenmore@gmail.com>
2026-08-24 15:57:35 +0530

    fix : secret issue

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR.parent / ".env")
+load_dotenv(BASE_DIR.parent / ".env", override=True)
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
